bcd524b681e30e8b809a9112d01a47e20e846c53
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-12-05 09:33:30 -0500

    Set up functional structure

--- src/clj_game/core.clj
+++ src/clj_game/core.clj
@@ -4,18 +4,52 @@
            (org.lwjgl.system MemoryUtil))
   (:gen-class))
 
-(defn open-window []
-  (let [window (GLFW/glfwCreateWindow 500 500 "Test" MemoryUtil/NULL MemoryUtil/NULL)]
-    (GLFW/glfwShowWindow window)
-    window))
+(def width 1280)
+(def height 720)
+(def game-title "Game")
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (GLFW/glfwInit)
-  (let [window (GLFW/glfwCreateWindow 640 480 "Test" MemoryUtil/NULL MemoryUtil/NULL)]
-    (GLFW/glfwMakeContextCurrent window)
-    (while (not (GLFW/glfwWindowShouldClose window))
-      (GLFW/glfwSwapBuffers window)
-      (GLFW/glfwPollEvents)))
+(defn setup
+  "Initial game setup setup. Returns the initial state"
+  []
+  ())
+
+(defn update-state
+  "Updates game state"
+  [state]
+  state)
+
+(defn render
+  "Renders the game"
+  [state]
+  ())
+
+(defn create-window [width height title]
+  (GLFW/glfwCreateWindow width height title MemoryUtil/NULL MemoryUtil/NULL))
+
+(defn setup-glfw
+  "Sets up GLFW options. Must have initiated GLFW and have a current OpenGL context."
+  [window]
+  (GLFW/glfwSwapInterval 1))
+
+(defn cleanup
+  "Cleans up and terminates the program"
+  []
   (GLFW/glfwTerminate))
+
+(defn game-loop
+  "The main game loop"
+  [window state]
+  (if (GLFW/glfwWindowShouldClose window)
+    (cleanup)
+    (do (render state)
+        (GLFW/glfwSwapBuffers window)
+        (GLFW/glfwPollEvents)
+        (game-loop window (update-state state)))))
+
+(defn -main [& args]
+  (GLFW/glfwInit)
+  (let [state (setup)
+        window (create-window width height game-title)]
+    (GLFW/glfwMakeContextCurrent window)
+    (setup-glfw window)
+    (game-loop window state)))
