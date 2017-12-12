9bebf689ae7d0da7af880b02829d327ecd1fc945
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-12-11 19:43:02 -0500

    Extract os-classifier to var

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
+(def os-classifier "natives-macos")
+
 (defproject clj-game "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
@@ -17,25 +19,25 @@
                  [org.lwjgl/lwjgl-vulkan "3.1.5"]
                  ; LWJGL Natives
                  [org.lwjgl/lwjgl "3.1.5"
-                  :classifier "natives-macos"]
+                  :classifier ~os-classifier]
                  [org.lwjgl/lwjgl-assimp "3.1.5"
-                  :classifier "natives-macos"]
+                  :classifier ~os-classifier]
                  [org.lwjgl/lwjgl-bgfx "3.1.5"
-                  :classifier "natives-macos"]
+                  :classifier ~os-classifier]
                  [org.lwjgl/lwjgl-glfw "3.1.5"
-                  :classifier "natives-macos"]
+                  :classifier ~os-classifier]
                  [org.lwjgl/lwjgl-nanovg "3.1.5"
-                  :classifier "natives-macos"]
+                  :classifier ~os-classifier]
                  [org.lwjgl/lwjgl-nuklear "3.1.5"
-                  :classifier "natives-macos"]
+                  :classifier ~os-classifier]
                  [org.lwjgl/lwjgl-openal "3.1.5"
-                  :classifier "natives-macos"]
+                  :classifier ~os-classifier]
                  [org.lwjgl/lwjgl-opengl "3.1.5"
-                  :classifier "natives-macos"]
+                  :classifier ~os-classifier]
                  [org.lwjgl/lwjgl-par "3.1.5"
-                  :classifier "natives-macos"]
+                  :classifier ~os-classifier]
                  [org.lwjgl/lwjgl-stb "3.1.5"
-                  :classifier "natives-macos"]]
+                  :classifier ~os-classifier]]
   :main ^:skip-aot clj-game.core
   :uberjar-name "game.jar"
   :jvm-opts ["-XstartOnFirstThread"]
