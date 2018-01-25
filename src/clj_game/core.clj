09d76d90137f0cd34e4cba8a302f98b5153f4c99
Jeremy Dormitzer <jdormitzer@hubspot.com>
2018-01-25 16:23:52 -0500

    Separate specific program rendering logic from general scaffolding

--- src/clj_game/core.clj
+++ src/clj_game/core.clj
@@ -11,43 +11,6 @@
 (def height 720)
 (def game-title "So Dope")
 
-(defn setup
-  "Initial game setup function. Returns the initial state."
-  []
-  (GL30/glBindVertexArray (GL30/glGenVertexArrays))
-  (let [vertices [-0.5  0.5  1.0  0.0  0.0
-                   0.5  0.5  0.0  1.0  0.0
-                   0.5 -0.5  0.0  0.0  1.0
-                  -0.5 -0.5  1.0  1.0  0.0]
-        indices [0 1 2
-                 2 3 0]
-        angle 0.0
-        vertex-buffer (opengl/gen-buffer :array-buffer)
-        indices-buffer (opengl/gen-buffer :element-array-buffer)
-        program (opengl/link-shaders [(shaders/basic-vertex-shader)
-                                      (shaders/basic-fragment-shader)])
-        angle-uniform (opengl/get-uniform program "angle")]
-    (GL20/glUseProgram program)
-    (opengl/load-data vertices :array-buffer :float :static-draw)
-    (opengl/load-data indices :element-array-buffer :byte :static-draw)
-    (opengl/set-uniform angle-uniform :float angle)
-    (opengl/link-attribute program "position" 2 :float false (* 5 Float/BYTES) 0)
-    (opengl/link-attribute program "color" 3 :float false (* 5 Float/BYTES) (* 2 Float/BYTES))
-    {:angle angle
-     :angle-uniform angle-uniform}))
-
-(defn update-state
-  "Updates game state."
-  [state]
-  (assoc state :angle (mod (+ (:angle state) 1) 360)))
-
-(defn render
-  "Renders the game."
-  [state]
-  (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
-  (opengl/set-uniform (:angle-uniform state) :float (:angle state))
-  (GL11/glDrawElements GL11/GL_TRIANGLES 6 GL11/GL_UNSIGNED_BYTE 0))
-
 (defn create-window [width height title]
   (GLFW/glfwDefaultWindowHints)
   (GLFW/glfwWindowHint GLFW/GLFW_VISIBLE GLFW/GLFW_FALSE)
@@ -78,23 +41,28 @@
 
 (defn game-loop
   "The main game loop."
-  [window state]
+  [window state update-state render]
   (if (GLFW/glfwWindowShouldClose window)
     (terminate)
     (do (render state)
         (GLFW/glfwSwapBuffers window)
         (GLFW/glfwPollEvents)
-        (game-loop window (update-state state)))))
+        (game-loop window (update-state state) update-state render))))
 
 (defn -main [& args]
-  (when-not (GLFW/glfwInit)
-    (throw (IllegalStateException. "Unable to initialize GLFW")))
-  (GLFW/glfwSetErrorCallback (GLFWErrorCallback/createPrint System/err))
-  (let [window (create-window width height game-title)]
-    (when (= window nil)
-      (throw (RuntimeException. "Error creating window")))
-    (GLFW/glfwMakeContextCurrent window)
-    (setup-opengl)
-    (setup-glfw window)
-    (let [state (setup)]
-      (game-loop window state))))
+  (require (symbol (first args)))
+  (let [ns-arg (first args)
+        setup (eval (symbol (str ns-arg "/setup")))
+        update-state (eval (symbol (str ns-arg "/update-state")))
+        render (eval (symbol (str ns-arg "/render")))]
+    (when-not (GLFW/glfwInit)
+     (throw (IllegalStateException. "Unable to initialize GLFW")))
+    (GLFW/glfwSetErrorCallback (GLFWErrorCallback/createPrint System/err))
+    (let [window (create-window width height game-title)]
+      (when (= window nil)
+        (throw (RuntimeException. "Error creating window")))
+      (GLFW/glfwMakeContextCurrent window)
+      (setup-opengl)
+      (setup-glfw window)
+      (let [state (setup)]
+        (game-loop window state update-state render)))))
