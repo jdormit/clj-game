fec7a28418fdd65abca349efa8c7b28165d4f26a
Jeremy Dormitzer <jdormitzer@hubspot.com>
2018-01-25 22:18:24 -0500

    Move shader code to live with individual programs

--- src/clj_game/spinningsquare.clj
+++ src/clj_game/spinningsquare.clj
@@ -1,7 +1,8 @@
 (ns clj-game.spinningsquare
-  (:require [clj-game.opengl :as opengl]
-            [clj-game.shaders :as shaders])
+  (:require [clj-game.opengl :as opengl])
   (:import (org.lwjgl.opengl GL11 GL30 GL20)))
+
+(declare vertex-shader fragment-shader)
 
 (defn setup []
   (GL30/glBindVertexArray (GL30/glGenVertexArrays))
@@ -14,8 +15,8 @@
         angle 0.0
         vertex-buffer (opengl/gen-buffer :array-buffer)
         indices-buffer (opengl/gen-buffer :element-array-buffer)
-        program (opengl/link-shaders [(shaders/basic-vertex-shader)
-                                      (shaders/basic-fragment-shader)])
+        program (opengl/link-shaders [(vertex-shader)
+                                      (fragment-shader)])
         angle-uniform (opengl/get-uniform program "angle")]
     (GL20/glUseProgram program)
     (opengl/load-data vertices :array-buffer :float :static-draw)
@@ -33,3 +34,36 @@
   (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
   (opengl/set-uniform (:angle-uniform state) :float (:angle state))
   (GL11/glDrawElements GL11/GL_TRIANGLES 6 GL11/GL_UNSIGNED_BYTE 0))
+
+(def vertex-shader
+  (opengl/make-shader
+   :vertex-shader
+   "#version 150 core
+
+    uniform float angle;
+
+    in vec2 position;
+    in vec3 color;
+
+    out vec3 Color;
+
+    void main () {
+        float angle_radians = angle * (3.1415926535/180);
+        mat2 rotation_matrix = mat2(cos(angle_radians), -sin(angle_radians),
+                                    sin(angle_radians), cos(angle_radians));
+        gl_Position = mat4(rotation_matrix) * vec4(position, 0.0, 1.0);
+        Color = color;
+    }"))
+
+(def fragment-shader
+  (opengl/make-shader
+   :fragment-shader
+   "#version 150 core
+
+    in vec3 Color;
+
+    out vec4 outColor;
+
+    void main() {
+        outColor = vec4(Color, 1.0);
+    }"))
